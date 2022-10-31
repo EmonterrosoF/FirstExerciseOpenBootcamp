1b926ba2a0449b9ac345687e8b81fcc658d0c889
EmonterrosoF <monterrosoedras.@gmail.com>
2022-10-30 22:07:34 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ejercicio 1 en openbootcamp
+
+mi primer ejercicio con openBootcamp
---
+++ src/com/project/openBootcamp/App.java
@@ -0,0 +1,41 @@
+package com.project.openBootcamp;
+/**
+ * @author Edras Monterroso
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        // enteros
+        byte bite = 127;
+        System.out.println(bite);
+
+        Short corto = 50;
+        System.out.println(corto);
+
+        int entero = 100;
+        System.out.println(entero);
+
+        long largo = 1000;
+        System.out.println(largo);
+
+        // decimales
+        float flotante = 5.3f;
+        System.out.println(flotante);
+
+        double decimal = 199.221;
+        System.out.println(decimal);
+
+        // booleanos
+
+        boolean verdadero = true;
+        System.out.println(verdadero);
+
+        // texto
+
+        char text = 'a';
+        System.out.println(text);
+
+        String cadena = "hello world";
+        System.out.println(cadena);
+
+    }
+}
